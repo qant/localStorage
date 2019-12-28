ccbc72d7077603c5e0cfb814ade5fd3f0d5608d7
Anton Semenov <anseme@gmail.com>
2019-12-28 10:45:07 +0100

    track form submit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,19 @@
 const twitslist = document.getElementById("twitslist");
 
 console.log(twitslist);
+
+//Events
+eventListener();
+
+function eventListener(e) {
+  //when form is submitted
+  document.querySelector("#form").addEventListener("submit", addTweet);
+}
+
+//Functions
+
+function addTweet(e) {
+  e.preventDefault();
+  console.info(e);
+  console.info("Form is submitted");
+}
